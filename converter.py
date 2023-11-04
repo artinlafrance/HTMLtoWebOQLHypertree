2aada4cec7e9f22032d03c3ba6d7e07a7699773b
artinlafrance <artin.lafrance@gmail.com>
2023-11-04 15:46:54 -0400

    Get the nodes source and text + additional changes

--- converter.py
+++ converter.py
@@ -6,5 +6,5 @@ with open('html_sample.html', 'r', encoding='utf-8') as file:
 tree = WebOQLTree(html_content)
 tree.format_file()
 tree.get_tag_node()
-print(tree.root.starting_tag_index)
-print(tree.root.closing_tag_index)
+tree.get_tags_text(tree.root, True)
+print(tree.stack[10].get_all_bracket_index())
--- tree.py
+++ tree.py
@@ -8,29 +8,43 @@ class Node:
         self.source = None
         self.text = None
 
-        self.starting_tag_index = None
-        self.closing_tag_index = None
+        self.open_bracket_starting_index = None
+        self.open_bracket_closing_index = None
+
+        self.closing_bracket_starting_index = None
+        self.closing_bracket_closing_index = None
 
     def set_tag(self, tag):
         self.tag = tag
+
+    
+    def set_open_bracket_index(self, startIndexPos, endIndexPos):
+        self.open_bracket_starting_index = startIndexPos
+        self.open_bracket_closing_index = endIndexPos
+
+
+    def set_closing_bracket_index(self, startIndexPos, endIndexPos):
+        self.closing_bracket_starting_index = startIndexPos
+        self.closing_bracket_closing_index = endIndexPos
 
     '''
     Formats the string and sets the source
     '''
     def set_source(self, source):
-        source = source[:45]
+        source = source[:100]
         source = source.replace("\n", "")
-        source += "..."
+        if source[-1] != '>':
+            source += "..."
         self.source = source
 
     def set_text(self, text):
         self.text = text
 
-    def set_starting_tag_index(self, index):
-        self.starting_tag_index = index
-
-    def set_closing_tag_index(self, index):
-        self.closing_tag_index = index
+    '''
+    Debug purposes : Prints the indexes of all the brackets.
+    '''
+    def get_all_bracket_index(self):
+        return f"Open bracket indexes : {self.open_bracket_starting_index}, {self.open_bracket_closing_index} \n Close bracket indexes : {self.closing_bracket_starting_index}, {self.closing_bracket_closing_index}"
 
     def print_node(self):
         return f"Tag: {self.tag} \n Source: {self.source} \n Text: {self.text}"
@@ -57,39 +71,73 @@ class WebOQLTree:
     '''
     Filters tag outs and creates the root node if it encounters an "html" tag
     '''
-    def filter_tags(self,tag, start_tag_index=None, closing_tag_index=None):
+    def filter_tags(self,tag, starting_index, closing_index):
         result_found = any(word in tag for word in self.filter_out)
         if not result_found:
-            #self.stack.append(tag)
-            if "html" in tag:
+            node = Node()
+            stack_index = self.check_tag_in_stack(tag[2:])
+            if "<html" in tag:
                 self.root = Node(tag)
-                self.root.set_starting_tag_index(start_tag_index)
-                self.root.set_closing_tag_index(closing_tag_index)
+                self.root.set_open_bracket_index(starting_index, closing_index)
                 self.stack.append(self.root)
+            elif "</html" in tag:
+                self.root.set_closing_bracket_index(starting_index, closing_index)
+                self.get_tags_source(self.root)
+            else:
+                #Push the tag 
+                if "<" in tag and "</" not in tag:
+                    node.set_tag(tag)
+                    node.set_open_bracket_index(starting_index, closing_index)
+                    self.stack.append(node)
+                if "</" in tag and stack_index != -1 :
+                    self.stack[stack_index].set_closing_bracket_index(starting_index, closing_index)
+                    self.get_tags_source(self.stack[stack_index])
+                    self.get_tags_text(self.stack[stack_index])
+    
 
+    '''
+    Checks to see if a substring (tag) is in the stack.
+    If it is it returns the Index. If not it returns false
+
+    Something needs to be fixed in this though
+    '''
+    def check_tag_in_stack(self, substring):
+        for index, node in enumerate(self.stack):    
+            if hasattr(node, 'tag') and substring in node.tag:
+                return index
+        return -1
+    
 
     def get_tag_node(self):
-        start_index_open = None
-        start_index_close = None
+        starting_index = None
         for index, char in enumerate(self.source):
-            if char == "</":
-                start_index_close = index
-            elif char == "<":
-                start_index_open = index
+            if char == "<":
+                starting_index = index
             elif char == ">":
-                if start_index_open is not None:
-                    tag = self.source[start_index_open:index+1]
-                    self.filter_tags(tag, start_index_open)
-                    start_index_open = None
-                if start_index_close is not None:
-                    tag = self.source[start_index_close:index+1]
-                    self.filter_tags(tag, start_index_close)
-                    start_index_close = None
+                if starting_index is not None:
+                    tag = self.source[starting_index:index+1]
+                    self.filter_tags(tag, starting_index, index)
+                    starting_index = None
 
 
     '''
-    This function takes in the starting index of the tag and the starting index of its closing tag
-    and iterates through the string to return its source.
+    This function takes in the starting index of the tag and the ending index of its closing tag
+    and gets the string between each index and sets its source
     '''
-    #def get_tags_source(self, start_tag_index, end_tag_index) -> str:
-        
+    def get_tags_source(self, Node:Node):
+        node_source = self.source[Node.open_bracket_starting_index:Node.closing_bracket_closing_index+1]
+        Node.set_source(node_source)
+
+
+    '''
+    This function searches for the Text portion of a Node 
+    '''
+    def get_tags_text(self, Node:Node, isRootNode = False):
+        string_to_search = self.source[Node.open_bracket_closing_index+1:Node.closing_bracket_starting_index]
+        if (isRootNode is True or "head" in Node.tag):
+            start_string_index = string_to_search.find("<title>")
+            end_string_index = string_to_search.find("</title>")
+            text = string_to_search[start_string_index+7:end_string_index]
+            Node.set_text(text)
+        else:
+            Node.set_text(string_to_search)
